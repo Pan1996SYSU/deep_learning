6487f4f26b591ea48493d2874cb50d7ad8d96fe1
Pan1996SYSU <1165645958@qq.com>
2023-02-17 19:59:44 +0800

    1

--- CNN.py
+++ CNN.py
@@ -1,34 +1,45 @@
 import torch
 
 
-class CNNnet(torch.nn.Module):
+class CNNNet(torch.nn.Module):
 
     def __init__(self):
-        super(CNNnet, self).__init__()
-        # [128, 1, 28, 28]->[128, 16, 14, 14]->[128, 32, 7, 7]->[128, 64, 4, 4]->
-        # [128, 64, 2, 2]->[128, 256]->[128, 100]->[128, 10]
+        super(CNNNet, self).__init__()
+        # [32, 3, 401, 401]->[32, 15, 200, 200]->[32, 30, 99, 99]->[32, 30, 49, 49]->
+        # [32, 30, 24, 24]->[32, 30, 11, 11]->[32, 30, 5, 5]->[32, 750]->
+        # [32, 300]->[32, 50]->[32, 2]
         # in_channels, out_channels, kernel_size, stride, padding
         self.conv1 = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 16, 3, 2, 1), torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(3, 15, 3, 2, 1), torch.nn.BatchNorm2d(15),
             torch.nn.ReLU())
         self.conv2 = torch.nn.Sequential(
-            torch.nn.Conv2d(16, 32, 3, 2, 1), torch.nn.BatchNorm2d(32),
+            torch.nn.Conv2d(15, 30, 3, 2, 1), torch.nn.BatchNorm2d(30),
             torch.nn.ReLU())
         self.conv3 = torch.nn.Sequential(
-            torch.nn.Conv2d(32, 64, 3, 2, 1), torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(30, 30, 3, 2, 1), torch.nn.BatchNorm2d(30),
             torch.nn.ReLU())
         self.conv4 = torch.nn.Sequential(
-            torch.nn.Conv2d(64, 64, 2, 2, 0), torch.nn.BatchNorm2d(64),
+            torch.nn.Conv2d(30, 30, 3, 2, 1), torch.nn.BatchNorm2d(30),
+            torch.nn.ReLU())
+        self.conv5 = torch.nn.Sequential(
+            torch.nn.Conv2d(30, 30, 3, 2, 1), torch.nn.BatchNorm2d(30),
+            torch.nn.ReLU())
+        self.conv6 = torch.nn.Sequential(
+            torch.nn.Conv2d(30, 30, 3, 2, 1), torch.nn.BatchNorm2d(30),
             torch.nn.ReLU())
         # in_features, out_features
-        self.mlp1 = torch.nn.Linear(256, 100)
-        self.mlp2 = torch.nn.Linear(100, 10)
+        self.mlp1 = torch.nn.Linear(750, 300)
+        self.mlp2 = torch.nn.Linear(300, 50)
+        self.mlp3 = torch.nn.Linear(50, 2)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
         x = self.mlp1(x.view(x.size(0), -1))
         x = self.mlp2(x)
+        x = self.mlp3(x)
         return x
--- train_cnn.py
+++ train_cnn.py
@@ -1,33 +1,63 @@
+from pathlib import Path
+
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torchvision
 from torch.autograd import Variable
 from torch.utils import data
 from torchvision.datasets import mnist
 
-from CNN import CNNnet
+from CNN import CNNNet
 
-data_tf = torchvision.transforms.Compose(
-    [
-        torchvision.transforms.ToTensor(),
-        torchvision.transforms.Normalize([0.5], [0.5])
-    ])
+# data_tf = torchvision.transforms.Compose(
+#     [
+#         torchvision.transforms.ToTensor(),
+#         torchvision.transforms.Normalize([0.5], [0.5])
+#     ])
+#
+# data_path = r'.\DATA'
+#
+# train_data = mnist.MNIST(
+#     data_path, train=True, transform=data_tf, download=False)
+# test_data = mnist.MNIST(
+#     data_path, train=False, transform=data_tf, download=False)
+#
+# train_loader = data.DataLoader(train_data, batch_size=300, shuffle=True)
+# test_loader = data.DataLoader(test_data, batch_size=100, shuffle=True)
+from utils_func import glob_extensions, cv_imread
 
-data_path = r'.\DATA'
-
-train_data = mnist.MNIST(
-    data_path, train=True, transform=data_tf, download=False)
-test_data = mnist.MNIST(
-    data_path, train=False, transform=data_tf, download=False)
-
-train_loader = data.DataLoader(train_data, batch_size=300, shuffle=True)
-test_loader = data.DataLoader(test_data, batch_size=100, shuffle=True)
-
-model = CNNnet()
+model = CNNNet()
 # model = torch.load(r'.\pth\4.pth')
 
 loss_func = torch.nn.CrossEntropyLoss()
 opt = torch.optim.Adam(model.parameters(), lr=0.001)
+train_loader = []
+test_loader = []
+train_path = r"C:\Users\MasterZ\Desktop\cat-dog-all-data\test-dataset\train"
+test_path = r"C:\Users\MasterZ\Desktop\cat-dog-all-data\test-dataset\test"
+train_path_list = glob_extensions(train_path)
+test_path_list = glob_extensions(test_path)
+for path in train_path_list[:100]:
+    path = Path(path)
+    img = cv_imread(path)
+    img = cv2.resize(img, (401, 401))
+    img = np.transpose(img, (2, 0, 1))
+    if path.parent.name == 'cat':
+        train_loader.append([torch.tensor(img), torch.tensor([1, 0])])
+    else:
+        train_loader.append([torch.tensor(img), torch.tensor([0, 1])])
+
+for path in test_path_list[:100]:
+    path = Path(path)
+    img = cv_imread(path)
+    img = cv2.resize(img, (401, 401))
+    mg = np.transpose(img, (2, 0, 1))
+    if path.parent.name == 'cat':
+        test_loader.append([torch.tensor(img), torch.tensor([1, 0])])
+    else:
+        test_loader.append([torch.tensor(img), torch.tensor([0, 1])])
 
 loss_count = []
 for epoch in range(5):
