ea31a07b80a73bd41ae388b3c907a6fc3ae185bc
潘伟志 <1165645958@qq.com>
2023-02-21 08:35:10 +0800

    Update RCNN.py

---
+++ RCNN.py
@@ -0,0 +1,13 @@
+
+
+suffix_xml = '.xml'
+suffix_jpg = '.jpg'
+
+
+car_train_path = './DATA/VOCdevkit/VOC2007/ImageSets/Main/car_train.txt'
+car_val_path = './DATA/VOCdevkit/VOC2007/ImageSets/Main/car_val.txt'
+
+vol_annotation_dir = './DATA/VOCdevkit/VOC2007/Annotations/'
+voc_jpg_dir = './DATA/VOCdevkit/VOC2007/JPEGImages'
+
+car_root_dir = './DATA/voc_car/'
