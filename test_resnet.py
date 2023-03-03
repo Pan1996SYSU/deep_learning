64d94d09bf66a567bc0bb5b6af45200e9cb35cef
潘伟志 <1165645958@qq.com>
2023-03-03 10:27:08 +0800

    1

--- test_resnet.py
+++ test_resnet.py
@@ -5,57 +5,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from my_dataset import CustomDataset
 from utils.utils_func import glob_extensions, cv_imread
 '''
 猫狗分类
 训练集：22500张
 测试集：2500张
-25张/批
-训练20代
+40张/批
+训练12代
 准确率：92.84%
 '''
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = torch.load(r'.\pth\ResNet_19.pth')
+model = torch.load(r'.\pth\ResNet_11.pth')
 accuracy_sum = []
 
 test_path = r".\DATA\\cat-dog-all-data\test-dataset\test"
-test_path_list = glob_extensions(test_path)
-random.shuffle(test_path_list)
-batch = 20
-for i, img_path in enumerate(test_path_list):
-    if i % batch != 0:
-        continue
-    else:
-        img = cv_imread(img_path)
-        img = cv2.resize(img, (401, 401))
-        img = np.transpose(img, (2, 0, 1))
-        img = torch.unsqueeze(torch.as_tensor(img, device=device), dim=0)
-        img = img.to(torch.float32)
-        batch_img = img
-        if Path(img_path).parent.name == 'cat':
-            y = torch.tensor([1, 0], device=device)
-        else:
-            y = torch.tensor([0, 1], device=device)
-        y = torch.unsqueeze(torch.as_tensor(y), dim=0)
-        y = y.to(torch.float32)
-        b_y = y
-        for j in range(1, batch):
-            img = cv_imread(test_path_list[i + j])
-            img = cv2.resize(img, (401, 401))
-            img = np.transpose(img, (2, 0, 1))
-            img = torch.unsqueeze(torch.as_tensor(img, device=device), dim=0)
-            img = img.to(torch.float32)
-            if Path(test_path_list[i + j]).parent.name == 'cat':
-                y = torch.tensor([1, 0])
-            else:
-                y = torch.tensor([0, 1])
-            y = torch.unsqueeze(torch.as_tensor(y, device=device), dim=0)
-            y = y.to(torch.float32)
-            batch_img = torch.cat((batch_img, img))
-            b_y = torch.cat((b_y, y))
-    test_x = Variable(batch_img)
-    test_y = Variable(b_y)
+dataset = CustomDataset(root_dir=test_path)
+dataloader = DataLoader(dataset, batch_size=30, shuffle=True)
+
+
+for i, (images, annotations) in enumerate(dataloader):
+    images = images.permute(0, 3, 2, 1).to(torch.float32)
+    images = images.to(device)
+    annotations = annotations.to(device)
+    test_x = Variable(images)
+    test_y = Variable(annotations)
     out = model(test_x)
     accuracy = torch.max(out.cpu(),
                          1)[1].numpy() == torch.max(test_y.cpu(),
