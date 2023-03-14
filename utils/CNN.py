2c694c98783caa877207913a48275ffcc8dd75a3
潘伟志 <1165645958@qq.com>
2023-03-14 10:46:37 +0800

    Update CNN.py

--- utils/CNN.py
+++ utils/CNN.py
@@ -40,7 +40,6 @@ class CNNNet(torch.nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
-
         x = self.conv3(x)
         x = self.conv4(x)
         x = self.conv5(x)
